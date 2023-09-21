d0519c9b2290423bdcc567f78edf3bdbd0b07ce2
Sơn Nguyễn Văn Thanh <sonnguyenvanthanh@192.168.1.17>
2023-09-21 23:50:51 +0700

    feat: 5.4 - init func p1

---
+++ contracts/marketplace/Marketplace.sol
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+//Library sup approve payment for all token use ERC20
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+
+contract Marketplace is Ownable {
+    using Counters for Counters.Counter;
+    using EnumerableSet for EnumerableSet.AddressSet;
+    // Khai báo các trường cần thiết cho 1 order
+    struct Order {
+        address seller;
+        address buyer;
+        uint256 tokenId;
+        address paymentToken;
+        uint256 price;
+    }
+    Counters.Counter private _orderIdCounter;
+    // immutable gíup nftContract trở thành 1 biến readOnly
+    // TODO
+    IERC721 public immutable nftContract;
+    mapping(uint256 => Order) orders;
+    uint256 public feeDecimal; //Phi giao dich
+    uint256 public feeRate; //Phí giá trị số nguyên
+    address public feeRecipient; // Địa chỉ ví
+    // approve payment for all token use ERC20
+    EnumerableSet.AddressSet private _supportedPaymentTokens;
+
+    // indexed có thể filter theo biến nào có đặt indexed
+    event OrderAdded(
+        uint256 indexed orderId,
+        address indexed seller,
+        uint256 indexed tokenId,
+        address paymentToken,
+        uint256 price
+    );
+
+    event OrderCancel(uint256 indexed orderId);
+
+    event OrderMatched(
+        uint256 indexed orderId,
+        address indexed seller,
+        address indexed buyer,
+        uint256 tokenId,
+        address paymentToken,
+        uint256 price
+    );
+
+    event FeeRateUpdated(uint256 feeDecimal, uint256 feeRate);
+
+    constructor(
+        address nftAddress_,
+        uint256 feeDecimal_,
+        uint256 feeRate_,
+        address feeRecipient_
+    ) {
+        require(
+            nftAddress_ != address(0),
+            "NFTMarketplace: nftAddress_ is zero address"
+        );
+
+        nftContract = IERC721(nftAddress_);
+        _updateFeRecipient(feeRecipient_);
+        _updateFeeRate(feeDecimal_, feeRate_);
+        _orderIdCounter.increment();
+    }
+
+    function _updateFeRecipient(address feeRecipient_) internal {
+        require(
+            feeRecipient_ != address(0),
+            "NFTMarketplace: feeRecipient is zero address"
+        );
+        feeRecipient = feeRecipient_;
+    }
+
+    function updateFeRecipient(address feeRecipient_) external onlyOwner {
+        _updateFeRecipient(feeRecipient_);
+    }
+
+    function _updateFeeRate(uint256 feeDecimal_, uint256 feeRate_) internal {
+        // Phần trăm fee bắt buộc phải nhỏ hơn 100%
+        require(
+            feeRate_ < 10 ** (feeDecimal_ + 2),
+            "NFTMarketplace: bad fee rate"
+        );
+        feeRate = feeRate_;
+        feeDecimal = feeDecimal_;
+        emit FeeRateUpdated(feeDecimal_, feeRate_);
+    }
+
+    function updateFeeRate(
+        uint256 feeDecimal_,
+        uint256 feeRate_
+    ) external onlyOwner {
+        _updateFeeRate(feeDecimal_, feeRate_);
+    }
+
+    function _calculateFee(uint256 orderId_) private view returns (uint256) {
+        // Lấy order
+        Order storage _order = orders[orderId_];
+        if (feeRate == 0) {
+            return 0;
+        }
+        return (feeRate * _order.price) / 10 ** (feeDecimal + 2);
+    }
+
+    function isSeller(
+        uint256 orderId_,
+        address seller_
+    ) public view returns (bool) {
+        return orders[orderId_].seller == seller_;
+    }
+}
